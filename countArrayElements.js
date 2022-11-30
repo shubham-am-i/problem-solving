b9ecb7c8f55f4f35cb77472ab57227d063400854
shubham-am-i <patilshubham2k21@gmail.com>
2022-11-30 19:37:00 +0530

    count occurence of element in an array

---
+++ countArrayElements.js
@@ -0,0 +1,15 @@
+function countElements(arr) {
+  let elementMap = {} //use charMap to store element and it's count
+
+  for (let curr of elements) {
+    if (!elementMap[curr]) {
+      elementMap[curr] = 1
+    } else {
+      elementMap[curr]++
+    }
+  }
+  return elementMap
+}
+
+const elements = [3, 43, 2, 56, 3, 8, 9, 3, 8, 9]
+console.log(countElements(elements))
